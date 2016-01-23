8a85f0aab3c4506efb0858c326e032084ffccafc
Matthew Page <matthewnpage@gmail.com>
2016-01-23 11:10:48 -0800

    demo - wip
    update demo torun with webpack-dev-server from root. npm run demo

--- src/Chart.js
+++ src/Chart.js
@@ -6,7 +6,7 @@ const Chart = React.createClass({
   propTypes: {
     height: React.PropTypes.number.isRequired,
     width: React.PropTypes.number.isRequired,
-    legend: React.PropTypes.arrayOf(React.PropTypes.object),
+    legend: React.PropTypes.object,
     children: React.PropTypes.arrayOf(React.PropTypes.object),
     margin: React.PropTypes.shape({
       top: React.PropTypes.number,
@@ -24,13 +24,41 @@ const Chart = React.createClass({
 
   render() {
     const { width, height, margin, viewBox, preserveAspectRatio, children, legend} = this.props;
-    const legendOffset = 0;
-    const showLegend = _.isArray(this.props.legend) && this.props.legend.length > 0;
+    let legendOffset = 0;
+    let showTopLegend = false;
+    let showBottomLegend = false;
+
+    if (_.isPlainObject(this.props.legend)){
+      if (_.isArray(this.props.legend.data) && this.props.legend.data.length > 0){
+        if (_.isString(this.props.legend.position)){
+          switch(this.props.legend.position.toLowerCase()){
+            case 'top':
+              showTopLegend = true;
+              break;
+            case 'bottom':
+              showBottomLegend = true;
+              break;
+            case 'both':
+              showTopLegend = true;
+              showBottomLegend = true;
+              break;
+            default:
+              showTopLegend = false;
+              showBottomLegend = false;
+              break;
+          }//end switch
+          if (showTopLegend || showBottomLegend){
+            legendOffset += 50;
+          }
+        }
+      }
+    }
     return (
       <div>
         <svg width={width} height={height+legendOffset} viewBox={viewBox} preserveAspectRatio={preserveAspectRatio} >
+          { showTopLegend && <Legend x={margin.left} y={margin.top} data={legend.data} /> }
           <g transform={`translate(${margin.left}, ${margin.top})`}>{children}</g>
-          { showLegend && <Legend data={legend} /> }
+          { showBottomLegend && <Legend x={margin.left} y={margin.top + height} data={legend.data} /> }
         </svg>
       </div>
     );
