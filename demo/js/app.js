d695637ec091520b2b78882d938696e0446a508d
Matt Page <matt.page@jivesoftware.com>
2016-01-30 08:34:24 -0800

    updated demo js

--- demo/js/app.js
+++ demo/js/app.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import BarChart from '../../src/BarChart.js';
+import LineChart from '../../src/LineChart.js';
+import '../css/main.scss';
 
-const data = [{
+const margin = {top: 10, bottom: 50, left: 50, right: 10};
+
+const chartData = [{
   label: 'somethingA',
   values: [{x: 'Apple', y: 10}, {x: 'Peaches', y: 4}, {x: 'Pumpkin Pie', y: 3}]
 }];
 
-const margin = {top: 10, bottom: 50, left: 50, right: 10};
-const legend = {
+const legendData = {
   position: 'bottom',
   data: [
     { 'apple': '#ff0000' },
@@ -17,7 +20,29 @@ const legend = {
 };
 
 React.render(
-  <BarChart data={data} height={400} width={400} margin={margin} legend={legend} />,
+  <div className='charts'>
+
+    <section className='chart'>
+      <h1>Bar Chart</h1>
+      <BarChart data={chartData} height={400} width={400} margin={margin} />
+    </section>
+
+    <section className='chart last'>
+      <h1>Bar Chart (with Legend)</h1>
+      <BarChart data={chartData} height={400} width={400} margin={margin} legend={legendData} />
+    </section>
+
+    <section className='chart'>
+      <h1>Line Chart</h1>
+      <LineChart data={chartData} height={400} width={400} margin={margin} />
+    </section>
+
+    <section className='chart last'>
+      <h1>Line Chart (with Legend)</h1>
+      <LineChart data={chartData} height={400} width={400} margin={margin} legend={legendData} />
+    </section>
+
+  </div>,
   document.getElementById('demo')
 );
 
