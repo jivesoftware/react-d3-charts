8e9c5e9d8947492731ade81374d525a6ea0e4eb0
Matt Page <matt.page@jivesoftware.com>
2016-02-04 17:16:55 -0800

    more barchart tests

--- test/BarChart-test.js
+++ test/BarChart-test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, render } from 'enzyme';
 import { expect } from 'chai';
 import BarChart from '../src/BarChart';
 
@@ -15,6 +15,22 @@ describe('<BarChart />', function() {
     const props = chart.props();
     expect(props.height).to.equal(height);
     expect(props.width).to.equal(width);
+  });
+
+  it('renders a <BarChart /> with some data', function(){
+    const data = [{
+      label: 'Fruits',
+      values: [{x: 'Apple', y: 10}, {x: 'Peaches', y: 4}, {x: 'Pumpkin Pie', y: 3}]
+    }];
+    const wrapper = shallow(<BarChart height={ height } width={ width } data={ data } />);
+    const chart = wrapper.find('.chart');
+    expect(chart).to.have.length(1);
+    const props = chart.props();
+    expect(props.height).to.equal(height);
+    expect(props.width).to.equal(width);
+    const html = wrapper.html();
+    expect(html).to.match(/g class="x axis"/);
+    expect(html).to.match(/rect class="bar"/);
   });
 
 });
