116ad523834f6aa2d6cac1d7109838c3c24230bd
Matt Page <matt.page@jivesoftware.com>
2016-02-05 09:56:04 -0800

    better area chart tests

--- test/AreaChart-test.js
+++ test/AreaChart-test.js
@@ -8,13 +8,29 @@ describe('<AreaChart />', function() {
   const width = 100;
 
   it('renders a <AreaChart /> component', function(){
-    const data = [ { label: 'apples', values: [{ x: 0, y: 0 }] } ];
+    const data = [];
     const wrapper = shallow(<AreaChart height={ height } width={ width } data={ data } />);
     const chart = wrapper.find('.chart');
     expect(chart).to.have.length(1);
     const props = chart.props();
     expect(props.height).to.equal(height);
     expect(props.width).to.equal(width);
+  });
+
+  it('renders a <AreaChart /> with some data', function(){
+    const data = [ {
+      label: 'Apple',
+      values: [{x: 0, y: 2}, {x: 1.3, y: 5}, {x: 3, y: 6}, {x: 3.5, y: 6.5}, {x: 4, y: 6}, {x: 4.5, y: 6}, {x: 5, y: 7}, {x: 5.5, y: 8}]
+    }];
+    const wrapper = shallow(<AreaChart height={ height } width={ width } data={ data } />);
+    const chart = wrapper.find('.chart');
+    expect(chart).to.have.length(1);
+    const props = chart.props();
+    expect(props.height).to.equal(height);
+    expect(props.width).to.equal(width);
+    const html = wrapper.html();
+    expect(html).to.match(/g class="x axis"/);
+    expect(html).to.match(/line x2=/);
   });
 
 });
