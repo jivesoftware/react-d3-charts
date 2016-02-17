a2c2627011887154e77ccc7d6d8588d7ce15a197
Matt Page <matt.page@jivesoftware.com>
2016-02-17 15:24:58 -0800

    support for legend text wrapping and better horizontal distribution of legend cells. fix #14

--- src/Legend.js
+++ src/Legend.js
@@ -1,6 +1,7 @@
 import React, { PropTypes, Component } from 'react';
 import d3 from 'd3';
 import color from 'sc-color';
+import _ from 'lodash';
 
 class Legend extends Component {
 
@@ -12,6 +13,7 @@ class Legend extends Component {
     symbolType: PropTypes.oneOf(['circle', 'cross', 'diamond', 'square', 'triangle-down', 'triangle-up']),
     symbolSize: PropTypes.number,
     symbolOffset: PropTypes.number,
+    wrapText: PropTypes.bool,
     x: PropTypes.number,
     y: PropTypes.number,
     defaultSymbolColor: PropTypes.string,
@@ -25,23 +27,85 @@ class Legend extends Component {
     cellTextClassName: 'label',
     symbolType: 'circle',
     symbolSize: 80,
-    symbolOffset: 50,
+    symbolOffset: 10,
+    wrapText: false,
     x: 0,
     y: 0,
     defaultSymbolColor: '#000000'
   };
+
+  componentDidMount() {
+    this.updateLegendText();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (!_.isEqual(this.props, prevProps)){
+      this.updateLegendText();
+    }
+  }
+
+  updateLegendText() {
+    const wrapper = function(text, width){
+      text.each(function(){
+        const t = d3.select(this);
+        if (!t.attr('data-wrapped')){
+          //split words on space, slashes, underscores, or dashes
+          const words = t.text().split(/[\s\/_-]+/).reverse();
+          if (words.length > 0){
+            let line = [];
+            let word;
+            let lineNumber = 0;
+            const lineHeight = 1.1; // ems
+            const x = '0.5em';
+            const y = '0.4em';
+            const dy = 0;
+            let textLen = 0;
+            let tspan = t.text(null).append('tspan').attr('x', x).attr('y', y).attr('dy', dy + 'em');
+            while (word = words.pop()) {
+              line.push(word);
+              tspan.text(line.join(' '));
+              textLen = tspan.node().getComputedTextLength();
+              if (textLen > width) {
+                line.pop();
+                tspan.text(line.join(' '));
+                line = [word];
+                t.attr('data-wrapped', 'true');
+                tspan = t.append('tspan').attr('x', x).attr('y', y).attr('dy', (lineNumber * lineHeight) + dy + 'em').text(word);
+                lineNumber += 1;
+              }
+            }
+          }
+        }
+      });
+    };
+
+    const leg = d3.select(this.refs.legend.getDOMNode());
+    if (this.props.wrapText){
+      leg.selectAll('.cell text').call(wrapper, d3.scale.ordinal().domain([]).range([]).rangeBand());
+    }
+
+    const symbolOffset = this.props.symbolOffset;
+    let textWidthOffset = 0;
+    const cells = leg.selectAll('g.cell');
+    cells.attr('transform', function (d, index) {
+      const cell = d3.select(this);
+      const box = cell.select('path').node().getBBox();
+      const textWidth = cell.select('text').node().getComputedTextLength();
+      const translate = `translate(${ ( (box.width + symbolOffset) * index ) + textWidthOffset }, 0)`;
+      textWidthOffset += textWidth;
+      return translate;
+    });
+  }
 
   render() {
     const cells = this.props.data.map((obj, index) =>{
       const symbolPathData = d3.svg.symbol().type(this.props.symbolType).size(this.props.symbolSize)();
       const keys = Object.keys(obj);
       const symbolColor = keys.length ? color(obj[keys[0]]).html() : color(this.props.defaultSymbolColor).html();
-      const cellTransform=`translate(${(this.props.symbolSize * index) + this.props.symbolOffset},0)`;
-
       return (
-        <g key={ this.props.cellClassName + index } className={this.props.cellClassName} transform={cellTransform}>
+        <g key={ this.props.cellClassName + index } className={this.props.cellClassName} >
           <path d={symbolPathData} style={{ fill: symbolColor }}></path>
-          <text className={this.props.cellTextClassName}>
+          <text className={this.props.cellTextClassName} textAnchor='start'>
             <tspan x='0.5em' y='0.4em' dy='0em'>{ keys[0] }</tspan>
           </text>
         </g>
@@ -50,7 +114,7 @@ class Legend extends Component {
 
     const legendTransform=`translate(${this.props.x}, ${this.props.y})`;
     return (
-      <g className={this.props.legendClassName}  transform={legendTransform}>
+      <g ref='legend' className={this.props.legendClassName}  transform={legendTransform}>
         <g className={this.props.cellsClassName}>
         { cells }
         </g>
